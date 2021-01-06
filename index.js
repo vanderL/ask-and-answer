324c85465b570f207a4f48cc2080162ba0aec7e6
Vander Lima <vnd.vander@gmail.com>
2021-01-06 14:39:19 -0300

    Partials and dados form delivery server

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.render('index')
 })
-
 app.get('/perguntar', (req,res) => {
     res.render("perguntar")
+})
+app.post('/salvarpergunta', (req, res) => {
+    var titulo = req.body.titulo
+    var description = req.body.description
+    res.send("Formulario recebido com sucesso!!!!!!!!!" + titulo + " - " + description)
 })
 
 app.listen(8080, ()=> {
